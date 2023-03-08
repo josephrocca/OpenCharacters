ae7d6650692546234193dc03fdbd73c20410586b
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-03-08 15:33:44 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -143,3 +143,12 @@ export function textToSpeech({text, voiceName}) {
     speechSynthesis.speak(utterance);
   });
 }
+
+
+export async function sha256Text(text) {
+  const msgUint8 = new TextEncoder().encode(text);                          
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);          
+  const hashArray = Array.from(new Uint8Array(hashBuffer));                    
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
