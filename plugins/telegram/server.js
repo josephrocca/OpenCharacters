330355a7f3afe35468240181819291e59473db85
Joe Holland <joseph@josephholland.ie>
2023-04-13 00:23:20 +0100

    Implemented Telegram bot integration

--- plugins/telegram/server.js
+++ plugins/telegram/server.js
@@ -1,14 +1,30 @@
-const app = require('express')();
-const http = require('http').Server(app);
+// Import necessary modules
+const app = require('express')(); // Express web framework
+const http = require('http').Server(app); // HTTP server
 const io = require('socket.io')(http, {
   cors: {
     origin: "*",
     methods: ["*"]
   }
-});
-const port = process.env.PORT || 4000;
+}); // Socket.IO for real-time communication
+const port = process.env.PORT || 4000; // Port to run the server on
+require('dotenv').config();
 
-// Serve test-chat page
+const botToken = process.env.BOT_TOKEN; // Get bot token from environment variable
+//const chatId = '1549238685' // Private bot channel
+const chatId = '-904620196' // Test bot group chat
+
+const { Telegraf } = require('telegraf'); // Telegraf for Telegram bot integration
+const bot = new Telegraf(botToken); // Create a new Telegraf bot object
+
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username
+})
+
+// Launch the Telegram bot
+bot.launch();
+
+// Serve local-chat page for testing
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/local-chat.html');
 });
@@ -18,30 +34,43 @@ app.get('/', (req, res) => {
 //  const namespace = oc.character.name.toLowerCase();
 // then we use this in the set up for the socket connection:
 //   const socket = io(`http://127.0.0.1:4000/${namespace}`);
-const namespaces = io.of(/^\/\w+$/);
+const namespaces = io.of(/^\/\w+$/); // Create a Socket.IO namespace for each bot
 
+// Handle connections to each namespace
 namespaces.on('connection', (socket) => {
-  const namespace = socket.nsp;
+  const namespace = socket.nsp; // Get the namespace the user has connected to
   console.log(`A user has connected to ${namespace.name}`);
 
+  // Handle changes in namespace (e.g. when a user switches to a different bot)
   socket.on("namespace change", (namespace) => {
-    socket.leave(socket.room);
-    socket.join(namespace);
-    socket.room = namespace;
+    socket.leave(socket.room); // Leave the current room
+    socket.join(namespace); // Join the new room
+    socket.room = namespace; // Update the current room
     console.log(`User joined namespace ${namespace}`);
   });
 
+  // Listen for messages in the group chat
+  bot.on('message', (ctx) => {
+    const message = ctx.message.text;
+    console.log(`Received user message: ${message} in ${namespace.name}`);
+    namespace.emit('user message', message); // Send the message to all connected clients in the namespace
+  });
+
+  // Handle AI messages from the bot
   socket.on('ai message', (msg) => {
     console.log(`Received ai message: ${msg} in ${namespace.name}`);
     namespace.emit('ai message', msg);
+    bot.telegram.sendMessage(chatId, `${msg}`); // Send the message to Telegram
   });
 
+  // Handle user messages from the client
   socket.on('user message', (msg) => {
     console.log(`Received user message: ${msg} in ${namespace.name}`);
-    namespace.emit('user message', msg);
+    namespace.emit('user message', msg); // Emit the message to all clients in the namespace
   });
 });
 
+// Start the server
 http.listen(port, () => {
   console.log(`Telegram bot integration server running at http://127.0.0.1:${port}/`);
 });
