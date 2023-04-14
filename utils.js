3b2bd079d8b3c8039eed24d6cf40a356847fc780
josephrocca <1167575+josephrocca@users.noreply.github.com>
2023-04-14 16:02:19 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -554,8 +554,16 @@ export function addBackgroundToElement(element) {
 }
 
 
-
-
+export function importStylesheet(src) {
+  return new Promise(function (resolve, reject) {
+    let link = document.createElement('link');
+    link.href = src;
+    link.rel = 'stylesheet';
+    link.onload = () => resolve(link);
+    link.onerror = () => reject(new Error(`Style load error for ${src}`));
+    document.head.append(link);
+  });
+}
 
 
 
